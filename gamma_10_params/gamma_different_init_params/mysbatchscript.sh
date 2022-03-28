e4cd0a23ec6b964055ddc188e244ec72fd061038
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2022-03-28 22:48:18 +0200

    gamma

---
+++ gamma_10_params/gamma_different_init_params/mysbatchscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+              #SBATCH --cpus-per-task=3
+              #SBATCH --mem-per-cpu=1G
+              #SBATCH --time=2-0:0
+	export NUMBA_NUM_THREADS=1
+	/home/edoardopedicillo/miniconda3/bin/python3 /home/edoardopedicillo/gamma/quantum_classical_1Dgamma.py
