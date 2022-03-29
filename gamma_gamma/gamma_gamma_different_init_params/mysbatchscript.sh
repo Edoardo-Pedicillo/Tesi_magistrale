561ef5d07cf7db58865dee4ba467723ca93c929e
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2022-03-29 15:35:32 +0200

    1D distribution

---
+++ gamma_gamma/gamma_gamma_different_init_params/mysbatchscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+              #SBATCH --cpus-per-task=3
+              #SBATCH --mem-per-cpu=1G
+              #SBATCH --time=5-0:0
+	export NUMBA_NUM_THREADS=1
+	/home/edoardopedicillo/miniconda3/bin/python3 /home/edoardopedicillo/gamma_gamma_different_init_params/quantum_classical_1Dgamma.py
