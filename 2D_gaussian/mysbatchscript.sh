89bee0dfc87670aa9bb0ef9ba7af919ca99acdd0
Edoardo Pedicillo <edoardopedicillo@pcteor1.mi.infn.it>
2022-04-21 07:48:19 +0200

    data epochs

---
+++ 2D_gaussian/mysbatchscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+              #SBATCH --cpus-per-task=3
+              #SBATCH --mem-per-cpu=1G
+              #SBATCH --time=2-0:0
+	export NUMBA_NUM_THREADS=1
+	/home/edoardopedicillo/miniconda3/bin/python3 /home/edoardopedicillo/tesi/2D_gaussian/quantum_classical_2Dgamma.py --lr=0.1 --n_epochs=5000 
