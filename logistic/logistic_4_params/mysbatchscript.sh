0e9232b7df4bbee5061e2d479db787d536caaa90
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2022-06-22 22:04:25 +0200

    data

--- logistic/logistic_4_params/mysbatchscript.sh
+++ logistic/logistic_4_params/mysbatchscript.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-              #SBATCH --cpus-per-task=3
-              #SBATCH --mem-per-cpu=1G
-              #SBATCH --time=5-0:0
-	export NUMBA_NUM_THREADS=1
-	/home/edoardopedicillo/miniconda3/bin/python3 /home/edoardopedicillo/tesi/logistic/logistic_4_params/quantum_classical_1Dgamma.py
+#SBATCH --job-name=zero
+#SBATCH --ntasks=1
+#SBATCH --time=10-0:0
+#SBATCH --partition=general
+#SBATCH --exclude=fermi,bose,chandra,bethe,hawking,higgs,salam,schroedinger,maxwell,cooper,haas,maisner
+
+#SBATCH --array=0-3:1 #specify how many times you want a job to run, we have a total of 7 array spaces
+	
+export NUMBA_NUM_THREADS=1
+	
+srun python /home/edoardopedicillo/tesi/logistic/logistic_4_params/quantum_classical_1Dgamma.py --iterator=$SLURM_ARRAY_TASK_ID
 
